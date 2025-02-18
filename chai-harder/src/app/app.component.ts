3521d891e9e575f7046e12aa3f3a065ce4efe9f4
Naomi Cox <46731849+NaomiCox9494@users.noreply.github.com>
2025-02-18 13:02:09 -0700

    start of styles

--- chai-harder/src/app/app.component.ts
+++ chai-harder/src/app/app.component.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { GridComponent } from './grid/grid.component';
+import {PageEvent, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    GridComponent,
+    MatSlideToggleModule,
+    MatPaginatorModule,
+    JsonPipe,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'chai-harder';
+
+  private _formBuilder = inject(FormBuilder);
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = false;
+
+  //example 
+  length = 18;
+  pageSize = 6;
+  pageIndex = 0;
+
+  hidePageSize = true;
+  showPageSizeOptions = true;
+  showFirstLastButtons = true;
+  disabled = false;
+
+  pageEvent: PageEvent | undefined;
+
+  handlePageEvent(e: PageEvent) {
+    this.pageEvent = e;
+    this.length = e.length;
+    this.pageSize = e.pageSize;
+    this.pageIndex = e.pageIndex;
+  }
 }
---
+++ chai-harder/src/app/grid/grid.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+
+@Component({
+  selector: 'app-grid',
+  templateUrl: './grid.component.html',
+  styleUrl: './grid.component.scss',
+  imports: [MatGridListModule],
+
+})
+export class GridComponent {
+
+}
